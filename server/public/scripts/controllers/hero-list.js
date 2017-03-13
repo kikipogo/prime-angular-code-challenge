f857686f53e69ec752611277569c1278bfa87aac
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-13 14:41:18 -0500

    uffda

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,32 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+
+    var self = this;
+    var hero = { list: [] };
+
+    getHero();
+
+    function getHero(){
+      $http({
+        method: 'GET',
+        url: '/heroes'
+      }).then(function(response){
+        console.log(response.data);
+        self.heroList = response.data;
+      });
+    }//end function
+
+   self.deleteHero = function(heroId){
+      console.log('delete was clicked');
+      console.log(heroId);
+      $http({
+        method: 'DELETE',
+        url: '/delete',
+        data: heroId
+      }).then(function(response){
+        
+        getHero();
+      })
+    }
+
+}]);
